e0aba2b9e715fad23408977ff1f682f00df9edc3
elisaquiney@gmail.com <elisaquiney@gmail.com>
2018-04-01 17:44:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ var recipes = {};
 function updateObjectWithKeyAndValue(recipes, key, value) {
   return Object.assign({},recipes, {[key]:value});
 }
+
+function destructivelyUpdateObjectWithKeyAndValue (recipes, key, value) {
+  recipes[key] = value;
+  return recipes;
+}
